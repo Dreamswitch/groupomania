c61c32e6f5c27a139b0281ce0f8057f620a1b529
Dreamswitch <jeambel.w@gmail.com>
2020-12-18 23:55:35 +0100

    add comments

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,15 +5,16 @@ const requestLimiter = require('../middlewares/requestLimiter');
 const auth = require('../middlewares/auth');
 const multer = require('../middlewares/multer-profil');
 
-
+// authentification 
 router.post('/signup', userCtrl.signup);
-router.post('/login',/* requestLimiter, */ userCtrl.login);
+router.post('/login', requestLimiter, userCtrl.login);
 
+// elevate privileges
+router.put('/privileges', auth, requestLimiter, userCtrl.grantPrivileges);
 
-router.get('/users', auth, /* requestLimiter, */ userCtrl.getAllUsers);
-router.put('/privileges', auth, /* requestLimiter, */ userCtrl.grantPrivileges);
-router.get('/profil', auth, /* requestLimiter, */ userCtrl.getProfile);
-router.put('/profil', auth, multer,/* requestLimiter, */ userCtrl.modifyProfile);
-router.delete('/profil', auth,/* requestLimiter, */ userCtrl.deleteProfile);
+router.get('/users', auth, requestLimiter, userCtrl.getAllUsers);
+router.get('/profil', auth, requestLimiter, userCtrl.getProfile);
+router.put('/profil', auth, multer, requestLimiter, userCtrl.modifyProfile);
+router.delete('/profil', auth, requestLimiter, userCtrl.deleteProfile);
 
 module.exports = router;
